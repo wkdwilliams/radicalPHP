7744a4daa718c4e10b66334bb33eeff9c67d0df3
Lewy <wkdwilliams@gmail.com>
2020-01-28 02:48:31 +0000

    Fix deprecations

--- Core/View.php
+++ Core/View.php
@@ -2,30 +2,19 @@
 
 namespace Core;
 
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
 class View
 {
-
-    public static function render($view, $args = [])
-    {
-        extract($args, EXTR_SKIP);
-
-        $file = dirname(__DIR__) . "/App/Views/$view";  // relative to Core directory
-
-        if (is_readable($file)) {
-            require $file;
-        } else {
-            throw new \Exception("$file not found");
-        }
-    }
-
     // Used for rendering a template using TWIG.
     public static function renderTemplate($template, $args = [])
     {
         static $twig = null;
 
         if ($twig === null) {
-            $loader = new \Twig_Loader_Filesystem(dirname(__DIR__) . '/App/Views');
-            $twig = new \Twig_Environment($loader);
+            $loader = new FilesystemLoader(dirname(__DIR__) . '/App/Views');
+            $twig = new Environment($loader);
         }
 
         echo $twig->render($template, $args);
